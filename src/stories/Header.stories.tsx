46f1543dd6a06beaee2277a4d9565d9c351f0696
Dave Tickle <dave.tickle@socialfinance.org.uk>
2023-06-16 11:28:47 +0100

    header stories

---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Header from '../components/header'
+
+export default {
+  title: 'Header',
+  component: Header,
+  argTypes: {
+    title: {
+      control: 'text',
+      defaultValue: 'Title!',
+    },
+    chip: {
+      control: 'text',
+      defaultValue: 'Chip!',
+    },
+  },
+} as ComponentMeta<typeof Header>
+
+const HeaderInst: ComponentStory<typeof Header> = (props) => {
+  return <Header {...props} />
+}
+
+export { HeaderInst as Header }
