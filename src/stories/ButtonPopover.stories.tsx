197413f6cc03574f34c0b8cf70add4c9c2df9ab5
Dave Tickle <dave.tickle@socialfinance.org.uk>
2023-06-16 10:58:50 +0100

    button popover stories

---
+++ src/stories/ButtonPopover.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import ButtonPopover from '../components/buttonpopover'
+
+export default {
+  title: 'Button Popover',
+  component: ButtonPopover,
+  argTypes: {
+    label: {
+      control: 'text',
+      defaultValue: 'Popover Label!',
+    },
+  },
+} as ComponentMeta<typeof ButtonPopover>
+
+const ButtonPopoverInst: ComponentStory<typeof ButtonPopover> = (props) => {
+  const allProps = {
+    children: <div>Hello, world!</div>,
+    ...props,
+  }
+
+  return <ButtonPopover {...allProps} />
+}
+
+export { ButtonPopoverInst as ButtonPopover }
