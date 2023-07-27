2b178cf3d5f763adeb0a525a70144c6485ed45a7
Dave Tickle <dave.tickle@socialfinance.org.uk>
2023-07-27 11:47:23 +0100

    feat: adding base file listing component and state management

--- src/components/filelist/FileList.tsx
+++ src/components/filelist/FileList.tsx
@@ -1,36 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemText from '@mui/material/ListItemText'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import Checkbox from '@mui/material/Checkbox'
-import DescriptionIcon from '@mui/icons-material/Description'
 
-const FileList = (): JSX.Element => {
+import FileItem from './FileItem'
+
+export type FileItemType = {
+  name: string
+  size: number
+  type?: 'doc' | 'xls' | 'csv' | 'pdf'
+}
+
+interface FileListProps {
+  fileList: FileItemType[]
+  onChange: (fileList: FileItemType[]) => void
+}
+
+const FileList = (props: FileListProps): JSX.Element => {
+  const [selectedItems, setSelectedItems] = useState<string[]>([])
+
+  const { fileList, onChange } = props
+
+  useEffect(() => {
+    setSelectedItems(
+      selectedItems.filter((selectedItem) => {
+        let output = false
+
+        fileList.forEach((fileListItem) => {
+          if (`${fileListItem.name}-${fileListItem.size}` === selectedItem) {
+            output = true
+          }
+        })
+
+        return output
+      })
+    ) // if we've done something externally to selected items, we need to ensure the internal selected list continues to reflect reality.
+  }, [fileList])
+
+  const getFilesByKeys = (keys: string[]) => {
+    const output: FileItemType[] = []
+
+    keys.forEach((key) => {
+      const [name, size] = key.split('-')
+
+      const found = fileList.find((fileItem) => {
+        return fileItem.name === name && fileItem.size === Number(size)
+      })
+      if (found) {
+        output.push(found)
+      }
+    })
+
+    return output
+  }
+
   return (
     <div>
       <List>
-        <ListItem
-          secondaryAction={
-            <Checkbox
-              edge="end"
-              onChange={() => {
-                console.log('')
-              }}
-              checked={false}
-              inputProps={{ 'aria-labelledby': '...' }}
-            />
-          }
-        >
-          <ListItemButton>
-            <ListItemIcon>
-              <DescriptionIcon />
-            </ListItemIcon>
+        {fileList.map((fileItem: FileItemType) => {
+          const key = `${fileItem.name}-${fileItem.size}`
 
-            <ListItemText primary="File" />
-          </ListItemButton>
-        </ListItem>
+          return (
+            <FileItem
+              selected={selectedItems.indexOf(key) > -1}
+              onChange={() => {
+                const newSelectedItems =
+                  selectedItems.indexOf(key) > -1
+                    ? selectedItems.filter((item) => {
+                        return item !== key
+                      })
+                    : selectedItems.slice().concat([key])
+
+                setSelectedItems(newSelectedItems)
+                onChange(getFilesByKeys(newSelectedItems))
+              }}
+              key={key}
+              {...fileItem}
+            />
+          )
+        })}
       </List>
     </div>
   )
--- src/components/filelist/index.ts
+++ src/components/filelist/index.ts
@@ -1,1 +1,1 @@
-export { default } from './FileList'
+export { default, FileItemType } from './FileList'
